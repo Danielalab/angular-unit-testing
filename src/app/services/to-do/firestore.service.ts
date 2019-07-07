db007a0935636673d59c808c5a7981d604cd3049
Danielalab <olabolacrayola@hotmail.com>
2019-07-07 12:08:06 -0500

    adding methods add y get tasks to Firestore

--- src/app/services/to-do/firestore.service.ts
+++ src/app/services/to-do/firestore.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FirestoreService {
 
-  constructor() { }
+  constructor(private angularFirestore: AngularFirestore) { }
+
+  addNote(textTask: string) {
+    return this.angularFirestore.collection('notes').add({
+      text: textTask
+    })
+  }
+
+  getNotes() {
+    // .snapshotChanges() returns a DocumentChangeAction[], which contains
+    // a lot of information about "what happened" with each change.
+    return this.angularFirestore.collection('notes').snapshotChanges()
+      .pipe(
+        map(actions => actions.map(action => {
+          const data = action.payload.doc.data();
+          const id = action.payload.doc.id;
+          return { id, ...data };
+        }))
+      );
+  }
 }
