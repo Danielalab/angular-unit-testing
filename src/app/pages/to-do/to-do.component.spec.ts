9fcaece791219d9645c7aad4d85653cc30e3e866
Danielalab <olabolacrayola@hotmail.com>
2019-07-07 18:12:32 -0500

    adding tess to-do page & mock Angular Firestore

--- src/app/pages/to-do/to-do.component.spec.ts
+++ src/app/pages/to-do/to-do.component.spec.ts
@@ -1,6 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ToDoComponent } from './to-do.component';
+import { FormAddTaskComponent } from 'src/app/components/to-do/form-add-task/form-add-task.component';
+import { ItemToDoComponent } from 'src/app/components/to-do/item-to-do/item-to-do.component';
+import { ToDoListComponent } from 'src/app/components/to-do/to-do-list/to-do-list.component';
+import { AngularFirestore } from '@angular/fire/firestore';
+
+import { AngularFirestoreMock } from '../../__mocks__/angular-firestore-mock';
 
 describe('ToDoComponent', () => {
   let component: ToDoComponent;
@@ -8,7 +14,18 @@ describe('ToDoComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ToDoComponent ]
+      // declarando todos los componentes pertenecientes a esta pagina.
+      declarations: [
+        ToDoComponent,
+        FormAddTaskComponent,
+        ItemToDoComponent,
+        ToDoListComponent
+      ],
+      // declarando los provedores utilizados
+      providers: [
+        // Configurando el uso del mock de Angular Firestore
+        { provide: AngularFirestore, useValue: new AngularFirestoreMock() }
+      ]
     })
     .compileComponents();
   }));
