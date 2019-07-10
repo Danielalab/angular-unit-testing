444db10defb8adac2d35cc997a14b0ed836108e0
Danielalab <olabolacrayola@hotmail.com>
2019-07-10 14:19:30 -0500

    adding specs form-add-task component

--- src/app/components/to-do/form-add-task/form-add-task.component.spec.ts
+++ src/app/components/to-do/form-add-task/form-add-task.component.spec.ts
@@ -4,11 +4,15 @@ import { FormAddTaskComponent } from './form-add-task.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { FirestoreService } from 'src/app/services/to-do/firestore.service';
 import { MockFirestoreService } from 'src/app/__mocks__/FirestoreService-mock';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('FormAddTaskComponent', () => {
   let component: FormAddTaskComponent;
   let fixture: ComponentFixture<FormAddTaskComponent>;
   let firestoreService: FirestoreService;
+  let inputEl: DebugElement;
+  let formEl: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -29,6 +33,8 @@ describe('FormAddTaskComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
     firestoreService = TestBed.get(FirestoreService);
+    inputEl = fixture.debugElement.query(By.css('input'));
+    formEl = fixture.debugElement.query(By.css('form'))
   });
 
   it('should create', () => {
@@ -36,22 +42,36 @@ describe('FormAddTaskComponent', () => {
   });
 
   it('Deberia de guardar el valor del input "aprender testing" en newNote', () => {
-
+    inputEl.nativeElement.value = 'aprender testing';
+    expect(component.newNote.value).toBe('aprender testing');
   })
 
   it('Deberia de renderizar el valor de newNote en el input', () => {
-
+    component.newNote.setValue('aprender tests');
+    expect(inputEl.nativeElement.value).toBe('aprender tests');
   })
 
   it('Deberia de llamar a "onSubmitFormAddNote" al dar submit al form', () => {
-
+    spyOn(component, 'onSubmitFormAddNote');
+    formEl.triggerEventHandler('submit', null);
+    expect(component.onSubmitFormAddNote).toHaveBeenCalled();
   })
 
   it('Deberia llamar a "addTask" con el text "agregando task" al ejecutar "onSubmitFormAddNote"', () => {
-
+    spyOn(firestoreService, 'addTask');
+    component.newNote.setValue('agregando task');
+    component.onSubmitFormAddNote({ preventDefault: () => {} })
+    expect(firestoreService.addTask).toHaveBeenCalled();
+    expect(firestoreService.addTask).toHaveBeenCalledWith('agregando task');
   })
 
-  it('Deberia el resetear el valor del "inputEl" y "newNote" a "" al ejecutar "onSubmitFormAddNote"', () => {
-
+  it('Deberia el resetear el valor del "inputEl" y "newNote" al ejecutar "onSubmitFormAddNote"', () => {
+    component.newNote.setValue('agregando task');
+    expect(component.newNote.value).toBe('agregando task');
+    expect(inputEl.nativeElement.value).toBe('agregando task');
+    // ejecuto el metodo "onSubmitFormAddNote"
+    component.onSubmitFormAddNote({ preventDefault: () => {} });
+    expect(component.newNote.value).toBe('');
+    expect(inputEl.nativeElement.value).toBe('');
   })
 });
