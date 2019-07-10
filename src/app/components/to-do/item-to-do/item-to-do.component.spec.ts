a05e2b0fcb92318daa54e66e7fec740ad798f580
Danielalab <olabolacrayola@hotmail.com>
2019-07-10 14:35:16 -0500

    adding specs item-to-do component

--- src/app/components/to-do/item-to-do/item-to-do.component.spec.ts
+++ src/app/components/to-do/item-to-do/item-to-do.component.spec.ts
@@ -1,11 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ItemToDoComponent } from './item-to-do.component';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('ItemToDoComponent', () => {
   let component: ItemToDoComponent;
   let fixture: ComponentFixture<ItemToDoComponent>;
-
+  let aEl: DebugElement;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ ItemToDoComponent ]
@@ -17,9 +19,20 @@ describe('ItemToDoComponent', () => {
     fixture = TestBed.createComponent(ItemToDoComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    aEl = fixture.debugElement.query(By.css('a'));
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Deberia de renderizar todoData.text en el "a" tag', () => {
+    expect(aEl.nativeElement.textContent.trim()).toBe('');
+    component.todoData = {
+      text: 'agregando input',
+      id: 'abc123'
+    };
+    fixture.detectChanges();
+    expect(aEl.nativeElement.textContent.trim()).toBe('agregando input')
+  })
 });
