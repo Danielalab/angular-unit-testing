b6629dcf48dd4f275ae04d5c2b9ca82e9d339ccf
Danielalab <olabolacrayola@hotmail.com>
2019-07-11 10:46:23 -0500

    adding routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ToDoComponent } from './pages/to-do/to-do.component';
+import { RankingSongsComponent } from './pages/ranking-songs/ranking-songs.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'to-do', component:  ToDoComponent},
+  { path: 'ranking-songs', component: RankingSongsComponent },
+  { path: '', redirectTo: '/to-do', pathMatch: 'full' },
+  { path: '**', redirectTo: '/to-do' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
